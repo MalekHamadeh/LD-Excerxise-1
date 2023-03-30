336b1e0ea4dc77bc86ed13ba3e2112dc75b8b60a
MalekHamdeh <malek_h98@outlook.com>
2023-03-30 13:50:52 +0300

    added routes to login page and main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Authentication from "./Pages/Authentication";
+import Home from "./Pages/Home";
 
 function App() {
-  return <div className='App'>Hello World!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' Component={Authentication} />
+        <Route exact path='/home' Component={Home} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Pages/Authentication.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Authentication = () => {
+  return <div>This is the authentication Page</div>;
+};
+
+export default Authentication;
